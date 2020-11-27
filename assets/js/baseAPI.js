c2854581b4c7c11e5fe8cfcdd8a3f2092f55f7b7
nick-luo <598855402@qq.com>
2020-11-27 15:14:09 +0800

    完成主页功能及退出功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,19 @@
 $.ajaxPrefilter(function(options) {
      // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-     options.url = 'http://ajax.frontend.itheima.net' + options.url
-    })
+     options.url = 'http://ajax.frontend.itheima.net' + options.url;
+
+    if(options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    options.complete = function(res) {
+        console.log(res);
+        if(res.responseJSON.message ==='身份认证失败！' && res.responseJSON.status ===1) {
+        localStorage.removeItem('token');
+        location.assign('/login.html')
+    
+    }
+    }
+
+})
---
+++ assets/js/index.js
@@ -0,0 +1,60 @@
+$(function() {
+
+    getUserinfo();
+
+    $('#logout').on('click', function() {
+        layui.layer.confirm('你确定要退出吗?', {icon: 3, title:'提示'}, function(index){
+            //do something
+            localStorage.removeItem('token');
+            location.href = '/login.html'
+            layer.close(index);
+          });
+    })
+
+
+
+
+
+
+
+function getUserinfo() {
+    $.ajax({
+        type: 'get',
+        url: '/my/userinfo',
+        // headers: {
+        //        Authorization: localStorage.getItem('token') || ''
+        //      },
+        success: function(res) {
+            // console.log(res);
+            if(res.status !== 0) {
+                return layui.layer.msg('获取数据失败');
+            }
+            renderAvator(res.data)
+        },
+        // complete: function(res) {
+        //     console.log(res);
+        //     if(res.responseJSON.message ==='身份认证失败！' && res.responseJSON.status ===1) {
+        //     localStorage.removeItem('token');
+        //     location.assign('/login.html')
+        
+        // }
+        // }
+    })
+}
+
+function renderAvator(user) {
+    var name = user.nickname || user.username;
+    $('.welcom').html('欢迎&nbsp;&nbsp;' + name);
+    if(user.user_pic) {
+        $('.layui-nav-img').prop('src', user.user_pic).show();
+        $('.text_avator').hide();
+    } else {
+        $('.layui-nav-img').hide();
+        var first = name[0].toUpperCase();
+        $('.text_avator').html(first).show();
+    }
+}
+
+
+
+})
